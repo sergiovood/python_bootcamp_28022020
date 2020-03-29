c2cb488cf03822e6ce3218b0c0ef926aa1058dc9
sergiusz <sergmanjob@gmail.com>
2020-03-29 11:11:35 +0200

    Zadanie_8

--- dzien_2/zadanie_8.py
+++ dzien_2/zadanie_8.py
@@ -2,6 +2,4 @@ w = int(input("Podaj długość długość: "))
 h = int(input("Podaj wysokośc opakowania: "))
 l = int(input("Podaj szerokość opakowania: "))
 
-v = l * w * h > 1
-
-print(v)
+print("Czy objętość większa od 1 litra?: ", l * w * h > 1000)
