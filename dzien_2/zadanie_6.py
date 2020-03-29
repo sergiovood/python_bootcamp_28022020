98e640e08d965e51bab20312eb5dd182f91d8190
sergiusz <sergmanjob@gmail.com>
2020-03-29 10:25:24 +0200

    Zadanie 6 zakończone

--- dzien_2/zadanie_6.py
+++ dzien_2/zadanie_6.py
@@ -1,10 +1,8 @@
 inp_user = int(input("Podaj Liczbę: "))
 
-a = inp_user > 10
-b = inp_user <= 15
-c = (inp_user % 2) == 0
+print(f"""
+Większa od 10: {inp_user > 10}
+Mniejsza równa 15: {inp_user <= 15}
+Podzielna przez 2: {(inp_user % 2) == 0}
+""")
 
-
-print(a)
-print(b)
-print(c)
