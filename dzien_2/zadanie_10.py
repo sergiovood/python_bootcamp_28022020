68ab2a66aa4fdf371b5faedccac51d6c7094b024
sergiusz <sergmanjob@gmail.com>
2020-03-29 12:50:36 +0200

    Zadanie_10

--- dzien_2/zadanie_10.py
+++ dzien_2/zadanie_10.py
@@ -1,14 +1,19 @@
-a = int(input("Podaj pierwszą liczbę: "))
-b = int(input("Podaj drugą liczbę: "))
-c = input("Podaj rodzaj operacji +, -, /, *: ")
+result = "Wynik: "
 
-if c == "+":
-    print(a + b)
-elif c == "-":
-    print(a - b)
-elif c == "/" and b != 0:
-    print(a / b)
-elif c == "*":
-    print(a * b)
+first = int(input("Podaj pierwszą liczbę: "))
+second = int(input("Podaj drugą liczbę: "))
+operation = input("Podaj rodzaj operacji +, -, /, *: ")
+
+if operation == "+":
+    print(result, first + second)
+elif operation == "-":
+    print(result, first - second)
+elif operation == "/":
+    if second == 0:
+        print("Nie można dzielić przez 0")
+    else:
+        print(result, first / second)
+elif operation == "*":
+    print(result, first * second)
 else:
     print("Nieprawidłowa operacja lub nie można dzielić przez 0")
