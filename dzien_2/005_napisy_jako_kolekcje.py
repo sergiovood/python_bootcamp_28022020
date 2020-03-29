038558ab119f177700f0e85c9be425090b3e95f4
sergiusz <sergmanjob@gmail.com>
2020-03-29 09:59:36 +0200

    WIP

--- dzien_2/005_napisy_jako_kolekcje.py
+++ dzien_2/005_napisy_jako_kolekcje.py
@@ -11,3 +11,15 @@ print(napis[10]) # wynik literka - s
 print(napis[-1]) #liczy w drugą strone tez wynik - s
 
 print(napis[3:7]) #wycinek napisu - od o do w
+
+
+
+inp_user = int(input("Podaj Liczbę: "))
+
+a = inp_user > 10
+b = inp_user <= 15
+c = inp_user % 2
+
+print(a)
+print(b)
+print(c)
