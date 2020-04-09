7edbd195096786b3998e9a5ce174f2e7e896e2e2
sergiusz <sergmanjob@gmail.com>
2020-04-09 14:53:43 +0200

    Zrobione

--- zadania_domowe/plansza.py
+++ zadania_domowe/plansza.py
@@ -1,25 +1,29 @@
-x_user = int(input("Podaj pozycję gracza X: "))
-y_user = int(input("Podaj pozycję gracza Y: "))
+t = "t"
+while t == "t":
+    x_user = int(input("Podaj pozycję gracza X: "))
+    y_user = int(input("Podaj pozycję gracza Y: "))
 
-pos = "Gracz znajduje się"
+    pos = "Gracz znajduje się"
 
-if x_user > 100 or y_user > 100:
-    print("Pozycja gracza poza planszą. Podaj wartości w przedziale od 1 do 100")
-elif x_user <= 10 and y_user <= 10:
-    print(pos, "w lewym dolnym rogu")
-elif x_user <= 10 and y_user >= 90:
-    print(pos, "w lewym górnym rogu")
-elif x_user < 10 and y_user > 10 and y_user < 90:
-    print(pos, "na lewej krawędzi")
-elif x_user > 10 and x_user < 90 and y_user < 10:
-    print(pos, "na dolnej krawędzi")
-elif x_user >= 90 and y_user <= 10:
-    print(pos, "w prawy dolnym rogu")
-elif x_user > 90 and y_user > 10 and y_user < 90:
-    print(pos, "na prawa krawędzi")
-elif x_user >= 90 and y_user >= 90:
-    print(pos, "w prawym górnym rogu")
-elif x_user > 10 and x_user < 90 and y_user > 90:
-    print(pos, "w górnej krawędzi")
-else:
-    print(pos, "w centrum")
+    if x_user > 100 or x_user < 0 or y_user > 100 or y_user < 0:
+        print("Pozycja gracza poza planszą. Podaj wartości w przedziale od 0 do 100")
+    elif x_user <= 10 and y_user <= 10:
+        print(pos, "w lewym dolnym rogu")
+    elif x_user <= 10 and y_user >= 90:
+        print(pos, "w lewym górnym rogu")
+    elif x_user >= 90 and y_user >= 90:
+        print(pos, "w prawym górnym rogu")
+    elif x_user >= 90 and y_user <= 10:
+        print(pos, "w prawy dolnym rogu")
+    elif x_user < 10 and y_user > 10 and y_user < 90:
+        print(pos, "na lewej krawędzi")
+    elif x_user > 10 and x_user < 90 and y_user < 10:
+        print(pos, "na dolnej krawędzi")
+    elif x_user > 90 and y_user > 10 and y_user < 90:
+        print(pos, "na prawa krawędzi")
+    elif x_user > 10 and x_user < 90 and y_user > 90:
+        print(pos, "w górnej krawędzi")
+    else:
+        print(pos, "w centrum")
+    t = input("Podac liczby jeszcze raz (t/n)? ")
+print("Koniec")
