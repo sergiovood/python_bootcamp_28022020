3836fe0e0666b97fdbbb1209dad5dd59da2f5afc
sergiusz <sergmanjob@gmail.com>
2020-04-12 21:47:33 +0200

    zarobione

--- zadania_domowe/z1.py
+++ zadania_domowe/z1.py
@@ -1,11 +1,7 @@
-usr_txt = input("Podaj tekst:")
-usr_txt = usr_txt.replace(" ", "")
-line1 = usr_txt[::2]
-line2 = usr_txt[1::2]
-margin = " "
-line1 = margin.join(line1)
-line2 = margin.join(line2)
+usr_txt = input("Podaj tekst:").replace(" ", "")
+line1 = " ".join(usr_txt[::2]).upper()
+line2 = " ".join(usr_txt[1::2]).upper()
 print(f"""
-{line1.upper()} 
-{line2.upper()}
+{line1} 
+{line2}
  """)
