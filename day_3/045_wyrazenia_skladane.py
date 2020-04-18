7581e8066c532c2192a93d25a0e21374d87e1b79
sergiusz <sergmanjob@gmail.com>
2020-04-18 16:09:50 +0200

    day_3

--- day_3/045_wyrazenia_skladane.py
+++ day_3/045_wyrazenia_skladane.py
@@ -6,7 +6,7 @@ print(x)
 szeszciany = []
 for el in x:
     szeszciany.append(el ** 3)
-print(szeszciany)
+print("szeszcian 1: ", szeszciany)
 
 print([el ** 3 for el in x])
 
@@ -15,7 +15,7 @@ szeszciany2 = []
 for el in x:
     if el % 2 == 0:
         szeszciany.append(el ** 3)
-print(szeszciany)
+print("szeszcian 2: ", szeszciany)
 
 ###
 print([[el, el ** 3] for el in x])
@@ -25,3 +25,7 @@ print({el ** 3 for el in x if el % 2 == 0})
 
 ###
 print(tuple(el ** 3 for el in x if el % 2 == 0))
+
+
+# zad 1
+print([x/10 for x in range(11)])
