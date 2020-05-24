184e231f8813b47fcf37f8a3efeb242cef2fe39b
sergiusz <sergmanjob@gmail.com>
2020-05-24 16:50:42 +0200

    Python 3.8

--- day_8/wyrazenia_regularne/regexp_examples_wyrazenia_regularne.py
+++ day_8/wyrazenia_regularne/regexp_examples_wyrazenia_regularne.py
@@ -1,7 +1,7 @@
 # wyrazenia regularne
 import re
 
-napis = "552-30312-345  123-34312-345 123-312-345 "
+napis = "552-30312-345  123-34312-345 123-312-345"
 
 kod_pocztowy_pattern = re.compile("\d{2}-\d{3}")
 data_pattern = re.compile("\d{2} \w{3} \d{4}")
