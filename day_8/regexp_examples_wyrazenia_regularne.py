df84557bc12cdcadc76201a4affe918a5c60671a
sergiusz <sergmanjob@gmail.com>
2020-05-24 16:38:39 +0200

    Python 3.8

---
+++ day_8/regexp_examples_wyrazenia_regularne.py
@@ -0,0 +1,15 @@
+# wyrazenia regularne
+import re
+
+napis = "552-30312-345  123-34312-345 123-312-345 "
+
+kod_pocztowy_pattern = re.compile("\d{2}-\d{3}")
+data_pattern = re.compile("\d{2} \w{3} \d{4}")
+email_pattern = re.compile("[\w\.\-]+@(?:\w+\.)+\w+")
+
+with open("input.txt") as f:
+    text = f.read()
+
+print(kod_pocztowy_pattern.findall(text))
+print(data_pattern.findall(text))
+print(email_pattern.findall(text))
